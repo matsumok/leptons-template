c5f296eedd5807a8c57731d920874635d8a4c6d0
matsumok <matsumok.digital@gmail.com>
2025-09-17 01:08:16 +0900

    Leptos + tailwindcss template

---
+++ src/app.rs
@@ -0,0 +1,69 @@
+use leptos::prelude::*;
+use leptos_meta::{provide_meta_context, MetaTags, Stylesheet, Title};
+use leptos_router::{
+    components::{Route, Router, Routes},
+    StaticSegment,
+};
+
+pub fn shell(options: LeptosOptions) -> impl IntoView {
+    view! {
+        <!DOCTYPE html>
+        <html lang="ja">
+            <head>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <AutoReload options=options.clone() />
+                <HydrationScripts options />
+                <MetaTags />
+            </head>
+            <body>
+                <App />
+            </body>
+        </html>
+    }
+}
+
+#[component]
+pub fn App() -> impl IntoView {
+    // Provides context that manages stylesheets, titles, meta tags, etc.
+    provide_meta_context();
+
+    view! {
+        // injects a stylesheet into the document <head>
+        // id=leptos means cargo-leptos will hot-reload this stylesheet
+        <Stylesheet id="leptos" href="/pkg/leptos-app.css" />
+
+        // sets the document title
+        <Title text="Welcome to Leptos" />
+
+        // content for this welcome page
+        <Router>
+            <main>
+                <Routes fallback=|| "Page not found.".into_view()>
+                    <Route path=StaticSegment("") view=HomePage />
+                </Routes>
+            </main>
+        </Router>
+    }
+}
+
+/// Renders the home page of your application.
+#[component]
+fn HomePage() -> impl IntoView {
+    // Creates a reactive value to update the button
+    let count = RwSignal::new(0);
+    let on_click = move |_| *count.write() += 1;
+
+    view! {
+        <div class="p-12 space-y-8">
+            <h1 class="text-2xl text-center">"Welcome to Leptos!"</h1>
+
+            <div class="flex justify-center">
+                <button class="dui-btn dui-btn-primary" on:click=on_click>
+                    "Click Me: "
+                    {count}
+                </button>
+            </div>
+        </div>
+    }
+}
